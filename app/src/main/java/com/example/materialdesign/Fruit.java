16dabfc9807fc740884f1e790f8e0ad23fe8d8c2
kevinzeng7430 <2472464767@qq.com>
2025-04-01 09:28:11 +0800

    MaterialDesign

---
+++ app/src/main/java/com/example/materialdesign/Fruit.java
@@ -0,0 +1,22 @@
+package com.example.materialdesign;
+
+public class Fruit {
+
+    private final String name;
+
+    private final int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
